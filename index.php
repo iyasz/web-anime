8bf4b786e3bdddb6768eeb7b7659084ba51469d8
Yasz Avellia <zakamaragames@gmail.com>
2022-09-02 20:38:41 +0700

    navbar sementara

--- index.php
+++ index.php
@@ -6,38 +6,54 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Classroom Of The Elite - Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Avestan&family=Open+Sans:wght@400;500&family=Roboto+Condensed&display=swap');
+
   .nav-item .nav-link {
-    color: white;
-    font-family: cursive;
+    color: #f9f9f9;
+    font-family: 'Noto Sans Avestan', sans-serif;
+    letter-spacing: 0.9px;
+    font-size: 15px;
+    border-radius: 6px;
+    margin-top: 5px;
+    padding-top: 3px;
+    padding-bottom: 3px;
+  }
+  .nav-item .nav-link:hover {
+    background-color: #3a3a3c;
+  }
+  .nav-item{
+    margin-left: 40px;
   }
 </style>
 
 <body>
 
-  <nav class="anime navbar navbar-dark navbar-expand-lg text-bg-dark py-2">
+  <nav class="navbar navbar-dark navbar-expand-lg text-bg-dark py-2">
     <div class=" container-fluid">
-      <a class="navbar-brand" href="">
-        <img src="asset/img/navlogo.png" alt="" width="110" height="44" class="d-inline-block align-text-top">
+      <a class="navbar-brand ms-3" href="">
+        <img src="asset/img/navlogo.png" alt="" width="110" height="44" class="d-inline-block align-text-top mb-0">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav ">
           <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#">Home </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Anime</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown link
+              Characters
             </a>
             <ul class="dropdown-menu dropdown-menu-dark">
               <li><a class="dropdown-item" href="#">Action</a></li>
@@ -45,15 +61,20 @@
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blogs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Studio</a>
+          </li>
+          <li class="nav-icon">
+            <a class="nav-link" href=""><i class='bx bxl-facebook-circle'></i></a>
+          </li>
         </ul>
-        <a href="" class="navbar-text">
-          Navbar text with an inline element
-        </a>
       </div>
     </div>
   </nav>
 
-  <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 
